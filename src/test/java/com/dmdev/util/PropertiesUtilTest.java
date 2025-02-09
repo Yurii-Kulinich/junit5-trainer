7f38d5205bb9e7315c50d82299556dffa452ae40
Yurii Kulinich <kulinich.yur@gmail.com>
2025-02-09 11:31:22 +0100

    first commit

---
+++ src/test/java/com/dmdev/util/PropertiesUtilTest.java
@@ -0,0 +1,13 @@
+package com.dmdev.util;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class PropertiesUtilTest {
+
+  @Test
+  void get() {
+    assertThat(true);
+  }
+}
